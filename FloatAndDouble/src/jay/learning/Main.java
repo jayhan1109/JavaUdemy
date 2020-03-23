081996ce7aaa36d6c3e059745b40d9530bf11b2e
Jeongho Han <jungho1109@gmail.com>
2020-03-22 19:42:58 -0700

    Float and Double challenge

---
+++ FloatAndDouble/src/jay/learning/Main.java
@@ -0,0 +1,30 @@
+package jay.learning;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+//        float myFloatValue = 5.25;
+        float myFloatValue = 5.25f;
+        float myFloatValue2 = (float) 5.25;
+        double myDoubleValue = 5.25;
+
+        myIntValue = 5 / 3;
+        myFloatValue = 5f / 3f;
+        myDoubleValue = 5d / 3d;
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+    }
+}
---
+++ FloatAndDoubleChallenge/src/jay/learning/Main.java
@@ -0,0 +1,11 @@
+package jay.learning;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double pound = 3.4d;
+        double pound_to_kilo = 0.45359237d;
+        double result = pound * pound_to_kilo;
+        System.out.println(result);
+    }
+}
