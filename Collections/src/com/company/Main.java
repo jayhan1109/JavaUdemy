3cb5c06176b1abd676d49d0f3fc5e3170283cc1e
Jeongho Han <jungho1109@gmail.com>
2020-04-01 15:43:05 -0700

    Collection - Initial

---
+++ Collections/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Theater theater = new Theater("Cineplex", 8,12);
+//	    theater.getSeats();
+	    if(theater.reserveSeat("H11")){
+            System.out.println("Please pay");
+        }else{
+            System.out.println("Sorry, seat is taken");
+        }
+
+        if(theater.reserveSeat("H11")){
+            System.out.println("Please pay");
+        }else{
+            System.out.println("Sorry, seat is taken");
+        }
+    }
+}
